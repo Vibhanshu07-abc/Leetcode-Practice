665c65b1172df36dfe4ac50c0c184d85e6993430
Vibhanshu07-abc <vibhanshuparmar555@gmail.com>
2026-06-28 23:13:57 +0530

    Time: 2 ms (80.89%), Space: 11.5 MB (43.23%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> freq(256, 0);
+        for (char ch : t)
+            freq[ch]++;
+        int left = 0;
+        int count = 0;
+        int start = -1;
+        int minLen = INT_MAX;
+        for (int right = 0; right < s.size(); right++) {
+            if (freq[s[right]] > 0)
+                count++;
+            freq[s[right]]--;
+            while (count == t.size()) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+                freq[s[left]]++;
+                if (freq[s[left]] > 0)
+                    count--;
+
+                left++;
+            }
+        }
+        return (start == -1) ? "" : s.substr(start, minLen);
+    }
+};
